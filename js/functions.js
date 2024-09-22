bff0e1f00701afc59930829ce11d06a100b1e82e
tuomaski <tuomas.kosunen02@gmail.com>
2024-09-22 22:21:29 +0300

    Noppa tehtävä

---
+++ js/functions.js
@@ -0,0 +1,4 @@
+document.getElementById('dice').addEventListener('click', function() {
+    const randomNumber = Math.floor(Math.random() * 6) + 1;
+    document.querySelector('#dice img').src = './img/' + randomNumber + '.png';
+});
